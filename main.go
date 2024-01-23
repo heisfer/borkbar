e51c5abcc7d856f8a8d41489bb7dedffb2de45cf
heisfer <heisfer@refract.dev>
2024-01-23 02:50:54 +0200

    basic clock implementation

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/diamondburned/gotk4-layer-shell/pkg/gtk4layershell"
+	"github.com/diamondburned/gotk4/pkg/glib/v2"
 	"github.com/diamondburned/gotk4/pkg/gtk/v4"
 )
 
@@ -24,13 +27,14 @@ func activate(app *gtk.Application) {
 	}
 
 	appwin := gtk.NewApplicationWindow(app)
+	clock(appwin)
 
 	window := &appwin.Window
 
 	gtk4layershell.InitForWindow(window)
 
+	// set position
 	gtk4layershell.SetLayer(window, gtk4layershell.Layer(gtk4layershell.LayerShellLayerBackground))
-
 	gtk4layershell.SetAnchor(window, gtk4layershell.Edge(gtk4layershell.LayerShellEdgeTop), true)
 	gtk4layershell.SetAnchor(window, gtk4layershell.Edge(gtk4layershell.LayerShellEdgeLeft), true)
 	gtk4layershell.SetAnchor(window, gtk4layershell.Edge(gtk4layershell.LayerShellEdgeRight), true)
@@ -41,4 +45,25 @@ func activate(app *gtk.Application) {
 	gtk4layershell.AutoExclusiveZoneEnable(window)
 
 	appwin.Show()
+}
+
+func clock(window *gtk.ApplicationWindow) {
+	clockLabel := gtk.NewLabel("clock place holder")
+	box := gtk.NewBox(gtk.OrientationHorizontal, 4)
+	box.Append(clockLabel)
+
+	window.SetChild(box)
+
+	go func() {
+		var ix int
+		for t := range time.Tick(time.Second) {
+			currentTime := t
+
+			ix++
+
+			glib.IdleAdd(func() {
+				clockLabel.SetLabel(fmt.Sprintf("Last updated at %s.", currentTime.Format(time.StampMilli)))
+			})
+		}
+	}()
 }
